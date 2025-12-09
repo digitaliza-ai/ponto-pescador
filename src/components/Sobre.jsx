f3ddf879d2a205388e5d774a20b6ac80b793c176
Gregory Soares <gregory16704@gmail.com>
2025-12-09 16:42:05 -0300

    style: corrigindo sobre nos

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -9,15 +9,7 @@ const Sobre = () => {
         <div className="sobre-text">
           <h3 className="local-name">Ponto do Pescador - Paranavaí</h3>
           <p className="descricao">
-            Bem-vindo ao Ponto do Pescador, o melhor lugar para desfrutar de peixes frescos 
-            e pratos deliciosos em Paranavaí. Nossa paixão pela culinária à base de peixe 
-            nos torna referência na região, oferecendo qualidade, sabor e um ambiente acolhedor 
-            para toda a família.
-          </p>
-          <p className="descricao">
-            Com anos de experiência, preparamos cada prato com carinho e dedicação, utilizando 
-            sempre os melhores ingredientes e técnicas tradicionais que garantem o sabor autêntico 
-            que nossos clientes tanto apreciam.
+            O Ponto do Pescador é a loja ideal para quem busca equipamentos de pesca e camping com o melhor custo-benefício de Paranavaí! Aqui, você não vai encontrar o mais caro nem o mais barato, mas sim produtos de qualidade por um preço justo. Nosso compromisso é oferecer opções acessíveis e funcionais para quem gosta de aproveitar a natureza sem gastar muito. Venha conferir nossa seleção e descubra como é fácil se equipar bem gastando pouco!
           </p>
         </div>
       </div>
